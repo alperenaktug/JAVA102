5d7d9485188c541f905298b946ec29557f8a7a62
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-05-27 21:00:45 +0300

    Final kavramı

---
+++ src/FinalKey/Daire.java
@@ -0,0 +1,16 @@
+package FinalKey;
+
+public class Daire {
+    public int r ;
+    public final double pi = 3.14;
+
+
+    public Daire(int r){
+        this.r = r ;
+    }
+    public void calcArea(){
+        double area = pi * (this.r * this.r);
+        System.out.println("Dairein Alanı : " + area);
+    }
+
+}
---
+++ src/FinalKey/Main.java
@@ -0,0 +1,10 @@
+package FinalKey;
+
+public class Main {
+    public static void main(String[] args) {
+        Daire d = new Daire(10);
+      //   d.pi = 200; Burada değişim yapamıyoruz.
+        d.calcArea();
+
+    }
+}
