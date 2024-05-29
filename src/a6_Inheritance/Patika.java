8b50395d23d15cecf45e9712743d85adb431d33a
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-05-29 22:49:43 +0300

    inheritance tablo

---
+++ src/a6_Inheritance/Patika.java
@@ -0,0 +1,16 @@
+package a6_Inheritance;
+
+public class Patika {
+    public static void main(String[] args) {
+
+        Calisan c1 = new Calisan("Alperen Aktuğ " , "05078656214" ,"alperaktug123@gmail.com");
+
+        //Akademisyen a1 = new Akademisyen("Polat Alemdar " , "05356636923", "polatalemdar12@gmail.com");
+
+       // a1.cikis();
+
+        Calisan m1 = new Memur("Demir Görkemli " , "05078656423" , "demir@gmail.com" , "Bilgi işlem" , "09-17");
+
+        m1.yemekhane();
+    }
+}
